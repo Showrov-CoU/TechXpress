10c98a3cb1d9b9d7c0a4becc679505da6b86e1f2
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-10-18 21:01:51 +0600

    added a product form for add product

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,9 +1,45 @@
-import PropTypes from "prop-types";
-
-const AddProduct = (props) => {
+const AddProduct = () => {
   return (
     <div>
-      <h1>Add</h1>
+      <h1 className="text-center text-3xl text-neutral font-bold">
+        Add Products
+      </h1>
+      <p className="text-center">
+        Expanding Our Range, One Product at a Time, Your Gateway to Fresh
+        Possibilities.
+      </p>
+      <form className="px-52 space-y-2">
+        <input
+          type="text"
+          placeholder="Image URL"
+          className="input input-bordered input-success w-full block"
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          className="input input-bordered input-success w-full block"
+        />
+        <input
+          type="text"
+          placeholder="Brand Name"
+          className="input input-bordered input-success w-full block"
+        />
+        <input
+          type="text"
+          placeholder="Price"
+          className="input input-bordered input-success w-full block"
+        />
+        <input
+          type="text"
+          placeholder="Rating"
+          className="input input-bordered input-success w-full block"
+        />
+        <textarea
+          className="textarea textarea-success block w-full"
+          placeholder="Short Description"
+        ></textarea>
+        <button className="signbtn text-neutralSilver w-full">Add</button>
+      </form>
     </div>
   );
 };
